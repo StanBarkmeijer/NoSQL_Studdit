06c68e16795362d99834e9cdf8283279f2626be3
Stan <stanbarkmeijer@hotmail.com>
2023-11-15 11:24:14 +0100

    Thread create controller

--- studdit/apps/studdit/src/app/threads/threads.controller.ts
+++ studdit/apps/studdit/src/app/threads/threads.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, NotFoundException, Post, ValidationPipe } from '@nestjs/common';
+import { ApiCreatedResponse, ApiNotFoundResponse, ApiTags, ApiUnprocessableEntityResponse } from '@nestjs/swagger';
+import { ThreadsService } from './threads.service';
+import { CreateThreadDto } from './dto/create-thread.dto';
+import { Thread } from './schemas/threads.schema';
 
+@ApiTags('threads')
 @Controller('threads')
-export class ThreadsController {}
+export class ThreadsController {
+    constructor(private readonly threadsService: ThreadsService) {}
+
+    @Post()
+    @ApiCreatedResponse({ description: 'The thread has been successfully created.'})
+    @ApiUnprocessableEntityResponse({ description: 'Unable to create thread.'})
+    @ApiNotFoundResponse({ description: 'User not found'})
+    async create(@Body(new ValidationPipe()) createThreadDto: CreateThreadDto): Promise<Thread> {
+        return await this.threadsService
+            .create(createThreadDto)
+            .then(thread => thread)
+            .catch(error => {
+                if (error instanceof NotFoundException) {
+                    throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+                }
+
+                throw new HttpException('Unable to create thread', HttpStatus.UNPROCESSABLE_ENTITY);
+            });
+    };
+}
